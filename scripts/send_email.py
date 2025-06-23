e6181be826faab482c42382e5881edcc18b3eea4
dani-ange <danielledjofang2003@gmail.com>
2025-06-23 22:31:26 +0200

    added emial script modif

---
+++ scripts/send_email.py
@@ -0,0 +1,50 @@
+from email.mime.text import MIMEText
+import smtplib
+import os
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+import json
+def send_email():
+    sender = os.environ["EMAIL_SENDER"]
+    receiver = os.environ["EMAIL_RECEIVER"]
+    password = os.environ["EMAIL_PASSWORD"]
+    smtp_server = os.environ["SMTP_SERVER"]
+    smtp_port = int(os.environ["SMTP_PORT"])
+
+    msg = MIMEMultipart()
+    msg['From'] = sender
+    msg['To'] = receiver
+    msg['Subject'] = subject
+    
+    # Attach the email body as plain text
+    msg.attach(MIMEText(body, "plain"))
+
+    # Attach the zip
+    part = MIMEBase('application', "octet-stream")
+    with open(zip_path, "rb") as file:
+        part.set_payload(file.read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', f'attachment; filename="{os.path.basename(zip_path)}"')
+    msg.attach(part)
+
+    with smtplib.SMTP(smtp_server, smtp_port) as server:
+        server.starttls()
+        server.login(sender, password)
+        server.sendmail(sender, receiver.split(','), msg.as_string())
+
+if __name__ == "__main__":
+    try:
+        with open("results.json", "r") as f:
+            results = json.load(f)
+        subject = "Model Training & Deployment Successful"
+        body = (
+            f"Model trained and evaluated successfully.\n"
+            f"📄 View hosted model at"
+            f" https://huggingface.co/danielle2003/normal-session"
+                    )
+
+    except Exception as e:
+        subject = "Model Training or Deployment Failed"
+        body = f"An error occurred:\n{e}"
+    send_email()
